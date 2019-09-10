6f8b5e643d1f453795ebbe72659788d7278f39a0
Jameson831 <jtwhite@mail.csuchico.edu>
2019-09-10 13:43:02 -0700

    My Working Madlibs program

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,38 @@
 //Authors: 
 #include <iostream>
 #include <string>
+using std:: string;
+using std::cin; 
+using std::cout; 
 
 int main()
 {
+ 
+ string firstName;
+ string Adjective;
+ string Noun;
+ int myNumber;
+ string adjective; 
+ 
+ 
+    cout<<" enter a first name: ";
+    cin>>firstName;
+    cout<<" Enter a Adjective: ";
+    cin>>Adjective;
+    cout<<" Enter a Noun: ";
+    cin>>Noun; 
+    cout<<" Enter a number: ";
+    cin>>myNumber;
+    cout<<" Enter a adjective: ";
+    cin>> adjective;
+
+    cout<<" Once apon a time two "<< firstName << " decided they wanted to " <<Adjective<< " a movie. The "<< Noun<< " of the movie " << myNumber << " so they decided "<<adjective<< " life was not the same ";
+    cout<< " decided they wanted to ";
+    
+   
+  
+ 
+
 
   return 0;
 }
